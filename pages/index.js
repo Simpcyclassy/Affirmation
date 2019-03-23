032a81957b55e940c7d1b6db925d59d4caaa5043
Simpcyclassy <chioma.onyekpere.185617@unn.edu.ng>
2019-03-23 09:13:03 +0100

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import DailyAffirmations from '../components/DailyAffirmations';
+  
+const Index = () => (
+  <div>
+    <DailyAffirmations/>
+  </div>
+)
+
+export default Index;
