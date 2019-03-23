0e23acff92b849e03ce52ddb48c30aa555384cff
Simpcyclassy <chioma.onyekpere.185617@unn.edu.ng>
2019-03-23 08:07:28 +0100

    antd set up

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import PositiveAffirmation from '../components/PositiveAffirmation';
+
+const Index = () => (
+  <div>
+    <PositiveAffirmation/>
+  </div>
+)
+
+export default Index;
