5497c00734965b5aa7192a66b506277cec28f6eb
Simpcyclassy <chioma.onyekpere.185617@unn.edu.ng>
2019-03-24 12:18:43 +0100

    antd d set up

--- components/DailyAffirmations.js
+++ components/DailyAffirmations.js
@@ -1,71 +1,50 @@
 import React, { Component } from 'react';
-// import { Radio} from 'antd';
+import Layout from './Layout';
+import { Radio} from 'antd';
 
-// export default class PositiveAffirmation extends Component {
-//     state = {
-//         affirmations: [],
-//         changeButton: "start",
-//     };
-
-//     toggleChangeButton = (e) => {
-//         this.setState ({
-//             changeButton: e.target.value,
-//         });
-//     }
-
-//   render() {
-//     return (
-//       <div>
-//         <Radio.Group buttonStyle="solid" onChange={this.toggleChangeButtone}>
-//             <Radio.Button value="start">Start</Radio.Button>
-//             <Radio.Button value="happy">Happy</Radio.Button>
-//             <Radio.Button value="sad">Sad</Radio.Button>
-//             <Radio.Button value="fullfiled">Fullfiled</Radio.Button>
-//         </Radio.Group>
-//       </div>
-//     );
-//   }
-// }
-
-import { Button, Radio, Icon } from 'antd';
-
-export default class DailyAffirmations extends Component {
+export default class PositiveAffirmation extends Component {
   state = {
-    size: 'large',
+    changeButton: false,
   };
 
-  handleSizeChange = (e) => {
-    this.setState({ size: e.target.value });
+  toggleChangeButton = e => {
+    this.setState ({
+      changeButton: e.target.value,
+    });
   }
 
   render() {
-    const size = this.state.size;
+    const { changeButton } = this.state;
     return (
-      <div>
-        <Radio.Group value={size} onChange={this.handleSizeChange}>
-          <Radio.Button value="large">Large</Radio.Button>
-          <Radio.Button value="default">Default</Radio.Button>
-          <Radio.Button value="small">Small</Radio.Button>
-        </Radio.Group>
-        <br /><br />
-        <Button type="primary" size={size}>Primary</Button>
-        <Button size={size}>Normal</Button>
-        <Button type="dashed" size={size}>Dashed</Button>
-        <Button type="danger" size={size}>Danger</Button>
-        <br />
-        <Button type="primary" shape="circle" icon="download" size={size} />
-        <Button type="primary" shape="round" icon="download" size={size}>Download</Button>
-        <Button type="primary" icon="download" size={size}>Download</Button>
-        <br />
-        <Button.Group size={size}>
-          <Button type="primary">
-            <Icon type="left" />Backward
-          </Button>
-          <Button type="primary">
-            Forward<Icon type="right" />
-          </Button>
-        </Button.Group>
-      </div>
+      <Layout>
+        <Radio.Group
+          defaultValue="false"
+          buttonStyle="solid"
+          onChange={this.changeButton}
+          className="radio-buttons"
+        >
+          <Radio.Button value={true}>Start</Radio.Button>
+          <Radio.Button value={false}>Stop</Radio.Button>
+        </Radio.Group>         
+        {
+          changeButton &&
+          <Button.Group size={size}>
+            <Button type="primary">Happy</Button>
+            <Button type="primary">Sad</Button>
+            <Button type="primary">Fullfiled</Button>
+          </Button.Group>         
+        }
+        <style jsx>{`
+          Radio.Group {
+            font-size: 100px;
+            margin-left: 20px;
+            margin-top: 5px;
+            margin-bottom: 5px;
+            display: flex;
+            justify-content: center;
+          }
+        `}</style>
+      </Layout>
     );
   }
 }
--- components/Layout.js
+++ components/Layout.js
@@ -1,21 +1,9 @@
 import Head from 'next/head'
-import { LocaleProvider } from 'antd'
-import enUS from 'antd/lib/locale-provider/en_US'
-
-export default ({ title, children }) =>
+export default ({children}) => (
   <div>
     <Head>
-      <title>{ title }</title>
-      <meta name='viewport' content='width=device-width, initial-scale=1' />
-      <meta charSet='utf-8' />
-      <link rel='stylesheet' href='//cdnjs.cloudflare.com/ajax/libs/antd/3.2.0/antd.min.css' />
+      <link rel='stylesheet' href='http://cdn.bootcss.com/antd/2.9.3/antd.css' />
     </Head>
-    <style jsx global>{`
-      body {
-      }
-    `}</style>
-    <LocaleProvider locale={enUS}>
-      <div>{children}</div>
-    </LocaleProvider>
+    {children}
   </div>
-  
+)
