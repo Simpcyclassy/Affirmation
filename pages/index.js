0d373e0d6cc7fb4d45c6277ad19f46d92ae0a619
Uche Nnadi <unnadi@pixsystem.com>
2019-03-25 01:07:26 -0700

    [add] antd styles to nextjs/react

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import stylesheet from 'antd/dist/antd.min.css'
+import { Layout, Menu, Breadcrumb, Icon } from 'antd';
 
 import DailyAffirmations from '../components/DailyAffirmations';
-  
-const Index = () => (
-  <div>
-    <DailyAffirmations/>
-  </div>
-)
 
-export default Index;
+export default class App extends React.Component {
+    render() {
+        return (
+            <Layout>
+                <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
+                <DailyAffirmations />
+            </Layout>
+        )
+    }
+}
