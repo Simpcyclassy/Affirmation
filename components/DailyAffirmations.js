65a114b7993f3cf91ab6d2281db95953ee68c7a8
Simpcyclassy <chioma.onyekpere.185617@unn.edu.ng>
2019-03-25 09:30:00 +0100

    progress

--- components/DailyAffirmations.js
+++ components/DailyAffirmations.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import Layout from './Layout';
-import { Radio} from 'antd';
+import HappyAffirmation from './HappyAffirmation';
+import { Radio, Button } from 'antd';
 
-export default class PositiveAffirmation extends Component {
+export default class DailyAffirmations extends Component {
   state = {
     changeButton: false,
   };
@@ -20,28 +21,33 @@ export default class PositiveAffirmation extends Component {
         <Radio.Group
           defaultValue="false"
           buttonStyle="solid"
-          onChange={this.changeButton}
+          onChange={this.toggleChangeButton}
           className="radio-buttons"
         >
           <Radio.Button value={true}>Start</Radio.Button>
-          <Radio.Button value={false}>Stop</Radio.Button>
-        </Radio.Group>         
+          <Radio.Button value={false}>Refocus your day</Radio.Button>
+        </Radio.Group>
         {
-          changeButton &&
-          <Button.Group size={size}>
-            <Button type="primary">Happy</Button>
-            <Button type="primary">Sad</Button>
-            <Button type="primary">Fullfiled</Button>
-          </Button.Group>         
-        }
+          changeButton && (
+          <Button.Group>
+            <HappyAffirmation />
+            <Button type="primary">Today I am sad</Button>
+            <Button type="primary">Today I am fullfiled</Button>
+            <Button type="primary">Today I am blessed</Button>
+          </Button.Group> 
+        )}
         <style jsx>{`
-          Radio.Group {
-            font-size: 100px;
-            margin-left: 20px;
-            margin-top: 5px;
-            margin-bottom: 5px;
-            display: flex;
-            justify-content: center;
+          // Radio.Group {
+          //   font-size: 100px;
+          //   margin-left: 20px;
+          //   margin-top: 5px;
+          //   margin-bottom: 5px;
+          //   display: flex;
+          //   justify-content: center;
+            margin-top: 0px;
+            margin-right: 0px;
+            margin-bottom: 0px;
+            margin-left: 0px;
           }
         `}</style>
       </Layout>
